3ec73125cfdcf9c9ae9be6d4ba6f043f629ac632
mdavidlau2006 <126034966+mdavidlau2006@users.noreply.github.com>
2023-03-25 19:10:13 -0400

    Create Flowers.java

---
+++ src/Flowers.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;  // Import the Scanner class
+
+class Main {
+  public static void main(String[] args) {
+    Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+    System.out.println("Entrar Dinero ");
+
+    float dinero = myObj.nextFloat();  // Read user input
+
+    if(dinero >= 120){
+        int Arreglo_A = (int)(dinero / 120);
+        int Arreglo_B = (int)(dinero / 70);
+        System.out.println("Puedes comprar " + Arreglo_A + " del arreglo floral A "  );
+        System.out.println("Puedes comprar " + Arreglo_B + " del arreglo floral B "  );
+    }
+    else{
+        int Arreglo_B = (int)(dinero / 70);
+        System.out.println("Puedes comprar " + Arreglo_B + " del arreglo floral B "  );
+    }
+  }
+}
+
